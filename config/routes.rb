488ac77bb53648aa9ade90bc343fb778348a64ba
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2018-10-23 14:56:58 +0800

    get update permission

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/test_update/', to: 'visitors#test_update'
+  get '/detail/', to: 'visitors#detail'
+  get '/show_tags/', to: 'visitors#show_tags'
   root to: 'visitors#index'
   devise_for :users
   resources :users
