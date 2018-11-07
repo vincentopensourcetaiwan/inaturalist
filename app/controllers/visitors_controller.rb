6746816fb8ee4f4cbea3d472a863023a9d273f92
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2018-11-07 11:24:39 +0800

    update tags

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -118,4 +118,135 @@ class VisitorsController < ApplicationController
     @tags = data["results"].last["description"].split(",")
 
   end
+
+  def add_tag
+    permitted = params.permit(:tag, :observation_id)
+    attributes = permitted.to_h || {}
+    attributes.values
+
+    @tag = attributes[:tag]
+    @observation_id = attributes[:observation_id]
+
+    # get api token
+    token = session[:token]
+    url = "https://www.inaturalist.org/users/api_token"
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request['Authorization'] = "Bearer #{token}"
+    response = http.request(request)
+    data = JSON.parse(response.body)
+    api_token = data["api_token"]
+
+    # get tags
+    url = "https://api.inaturalist.org/v1/observations/#{@observation_id}"
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    data = JSON.parse(response.body)
+    @tags = data["results"].last["description"].split(",")
+
+    # new tags
+    @tags << @tag
+    description = ""
+    @tags.each do |tag|
+      description = description + "#{tag}, "
+    end
+    description.chop!.chop!
+
+    url = "https://api.inaturalist.org/v1/observations/#{@observation_id}"
+    uri = URI.parse(url)
+    request = Net::HTTP::Put.new(uri)
+    request.content_type = "application/json"
+    request["Accept"] = "application/json"
+    request["Authorization"] = api_token
+    request.body = JSON.dump({
+                               "ignore_photos" => 1,
+                               "observation" => {
+                                 "description" => description
+                               }
+                             })
+
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+    redirect_to show_tags_path(observation_id: @observation_id)
+  end
+
+
+  def delete_tag
+    permitted = params.permit(:tag, :observation_id)
+    attributes = permitted.to_h || {}
+    attributes.values
+
+    @tag = attributes[:tag].strip!
+    @observation_id = attributes[:observation_id]
+
+    # get api token
+    token = session[:token]
+    url = "https://www.inaturalist.org/users/api_token"
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request['Authorization'] = "Bearer #{token}"
+    response = http.request(request)
+    data = JSON.parse(response.body)
+    api_token = data["api_token"]
+
+    # get tags
+    url = "https://api.inaturalist.org/v1/observations/#{@observation_id}"
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    data = JSON.parse(response.body)
+    @tags = data["results"].last["description"].split(",")
+
+    # new tags
+    @tags.each do |tag|
+      tag.strip!
+    end
+    @tags.delete(@tag)
+    description = ""
+    @tags.each do |tag|
+      description = description + "#{tag}, "
+    end
+    description.chop!.chop!
+
+    url = "https://api.inaturalist.org/v1/observations/#{@observation_id}"
+    uri = URI.parse(url)
+    request = Net::HTTP::Put.new(uri)
+    request.content_type = "application/json"
+    request["Accept"] = "application/json"
+    request["Authorization"] = api_token
+    request.body = JSON.dump({
+                               "ignore_photos" => 1,
+                               "observation" => {
+                                 "description" => description
+                               }
+                             })
+
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+    redirect_to show_tags_path(observation_id: @observation_id)
+
+  end
 end
