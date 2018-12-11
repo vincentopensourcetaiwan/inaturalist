e1cea817a76e63affc1562498afdabd96b0713c6
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2018-12-11 17:31:02 +0800

    iNaturalist API services

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -14,17 +14,7 @@ class VisitorsController < ApplicationController
     else
       convert = { :url => attributes[:keyword] }.to_query
       keyword = convert.split("=")[1]
-      url = "https://api.inaturalist.org/v1/observations?project_id=1ac02830-8817-404e-a6ab-86027362db9c&q=#{keyword}&order=desc&order_by=created_at&per_page=1000"
-      uri = URI.parse(url)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-      request = Net::HTTP::Get.new(uri.request_uri)
-
-      response = http.request(request)
-      data = JSON.parse(response.body)
-      @observations = data["results"]
+      @observations = InaturalistService.search(keyword)
     end
 
     permitted = params.permit(:keyword)
