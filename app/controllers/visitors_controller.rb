815cf295c0b8080728e345125552e1db78e77b5b
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2018-12-12 10:51:19 +0800

    remove code

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -16,30 +16,6 @@ class VisitorsController < ApplicationController
       keyword = convert.split("=")[1]
       @observations = InaturalistService.search(keyword)
     end
-
-    permitted = params.permit(:keyword)
-    attributes = permitted.to_h || {}
-    attributes.values
-
-    @keyword_string = attributes[:keyword]
-    if (@keyword_string.nil?) || (@keyword_string == "")
-      @resutls = []
-    else
-      convert = { :url => attributes[:keyword] }.to_query
-      keyword = convert.split("=")[1]
-      url = "https://api.inaturalist.org/v1/observations?project_id=1ac02830-8817-404e-a6ab-86027362db9c&q=#{keyword}&order=desc&order_by=created_at&per_page=1000"
-      uri = URI.parse(url)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-      request = Net::HTTP::Get.new(uri.request_uri)
-
-      response = http.request(request)
-      data = JSON.parse(response.body)
-      @resutls = data["results"]
-    end
-
   end
 
   def show_tags
