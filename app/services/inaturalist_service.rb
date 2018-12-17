f6d0b44e96fc84e6f18d6b4390909163acc042c7
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2018-12-17 15:49:36 +0800

    get API token service

--- app/services/inaturalist_service.rb
+++ app/services/inaturalist_service.rb
@@ -3,14 +3,16 @@ class InaturalistService
   require "uri"
   require "json"
 
-  INATURALIST_API_URL = "https://api.inaturalist.org/v1/observations"
+  API_URL = "https://api.inaturalist.org/v1/observations"
   NANHU_PROJECT_ID = "1ac02830-8817-404e-a6ab-86027362db9c"
   ORDER = "desc"
   ORDER_BY = "created_at"
   PER_PAGE = "1000"
+  API_TOKEN_URL = "https://www.inaturalist.org/users/api_token"
+  
 
   def self.search_observations(keyword)
-    url = "#{INATURALIST_API_URL}?project_id=#{NANHU_PROJECT_ID}&q=#{keyword}&order=#{ORDER}&order_by=#{ORDER_BY}&per_page=#{PER_PAGE}"
+    url = "#{API_URL}?project_id=#{NANHU_PROJECT_ID}&q=#{keyword}&order=#{ORDER}&order_by=#{ORDER_BY}&per_page=#{PER_PAGE}"
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
@@ -31,6 +33,19 @@ class InaturalistService
     response = http.request(request)
     data = JSON.parse(response.body)
     data["results"]
+  end
+
+  def self.get_api_token(token)
+    url = API_TOKEN_URL
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request['Authorization'] = "Bearer #{token}"
+    response = http.request(request)
+    data = JSON.parse(response.body)
+    data["api_token"]
   end
 
 end
