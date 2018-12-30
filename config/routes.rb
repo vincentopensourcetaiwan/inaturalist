53b426142c4640c8ca9602d0828c6cde23f440e9
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2018-12-30 12:12:16 +0800

    visitor's pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get '/search/', to: 'visitors#search'
+  get '/search_results/', to: 'visitors#search_results'
   get '/show_tags/', to: 'visitors#show_tags'
   post '/add_tag', to: 'visitors#add_tag'
   delete '/delete_tag', to: 'visitors#delete_tag'
-  root to: 'visitors#index'
+  root to: 'visitors#search'
   devise_for :users
   resources :users
 end
