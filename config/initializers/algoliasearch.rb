1797ebf3bd91110b52a5cd9182537ff90ea2f921
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2019-01-15 18:16:11 +0800

    pagination

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,1 +1,1 @@
-AlgoliaSearch.configuration = { application_id: ENV['ALGOLIASEARCH_APPLICATION_ID'], api_key: ENV['ALGOLIASEARCH_API_KEY'] }
+AlgoliaSearch.configuration = { application_id: ENV['ALGOLIASEARCH_APPLICATION_ID'], api_key: ENV['ALGOLIASEARCH_API_KEY'], pagination_backend: :kaminari }
