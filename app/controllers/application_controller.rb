ede8e1dc53e3d04113c3170bbede0c75908461d7
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2019-01-21 17:41:26 +0800

    Observation Admin panel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_in_path_for(resource)
-    admin_users_path
+    admin_observations_path
   end
 
   def after_sign_out_path_for(resource)
