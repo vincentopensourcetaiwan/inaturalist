80dabf6b810942f166a77826875ae7c0e415b5e7
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2019-01-26 11:36:15 +0800

    show observation author

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < ApplicationController
+  include Pundit
   layout "admin"
   before_action :authenticate_user!
   before_action :set_user, only: [:show, :edit, :update, :send_confirmation_instructions, :confirm]
