f327e26ba298a2e953f2b41a9f99b6cd87aa5e72
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2020-01-17 16:15:06 +0800

    Authenticate API

--- app/controllers/apis/v1/observations_controller.rb
+++ app/controllers/apis/v1/observations_controller.rb
@@ -1,4 +1,5 @@
 class Apis::V1::ObservationsController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @observations = Observation.enabled.search(permitted_params["keyword"]).order_by_id_desc
