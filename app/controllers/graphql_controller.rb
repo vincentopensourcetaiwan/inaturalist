131cbf84c893801ab6e78df76ad68cfcde70cd10
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2020-01-20 11:43:20 +0800

    Auth GraphQL API

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -3,6 +3,7 @@ class GraphqlController < ApplicationController
   # This allows for outside API access while preventing CSRF attacks,
   # but you'll have to authenticate your user separately
   # protect_from_forgery with: :null_session
+  before_action :authenticate_user!
 
   def execute
     variables = ensure_hash(params[:variables])
@@ -10,7 +11,7 @@ class GraphqlController < ApplicationController
     operation_name = params[:operationName]
     context = {
       # Query context goes here, for example:
-      # current_user: current_user,
+      current_user: current_user
     }
     result = InaturalistSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
