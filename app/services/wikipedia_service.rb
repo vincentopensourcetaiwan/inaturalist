68152c41cdbfe5c07502f7e133ea88eeb5b038e1
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2021-04-16 17:48:15 +0800

    fix test case

--- app/services/wikipedia_service.rb
+++ app/services/wikipedia_service.rb
@@ -1,18 +1,9 @@
 class WikipediaService
-  ONLY_ENGLISH_AND_NUMBER = /^[A-Za-z0-9 ]*$/
-
   def self.get_chinese_taxon_name(taxon_name)
-    url = "https://zh.wikipedia.org/zh-tw/#{taxon_name}"
-    real_url = URI.encode(url)
-    uri = URI.parse(real_url)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Get.new(uri.request_uri)
-    response = http.request(request)
+    url = "https://zh.wikipedia.org/zh-tw/#{taxon_name.gsub(" ", "%20")}"
+    response = Faraday.get url
     doc = Nokogiri::HTML(response.body)
-    name = doc.css("h1").first.text
-    name.match(ONLY_ENGLISH_AND_NUMBER).present? ? nil : name
+    return "" unless response.status == 200
+    doc.css("h1").first.text
   end
-
 end
